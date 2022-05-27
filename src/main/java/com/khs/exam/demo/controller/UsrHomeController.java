9ebb6da7d95cb58266ebe1ba7f7335710422366e
tmedcount <tmedcount@gmail.com>
2022-05-27 17:43:44 +0900

    UsrArticleController의 서비스 로직을 ArticleService로 옮김

--- src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
@@ -13,77 +13,10 @@ import com.khs.exam.demo.vo.Article;
 
 @Controller
 public class UsrHomeController {
-	@RequestMapping("usr/home/getString")
+	@RequestMapping("usr/home/main")
 	@ResponseBody
-	public String getString() {
-		return "Hi";
+	public String getMain() {
+		return "안녕하세요!";
 	}
 	
-	@RequestMapping("usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	@RequestMapping("usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("usr/home/getCharacter")
-	@ResponseBody
-	public char getCharacter() {
-		return 'a';
-	}
-	
-	@RequestMapping("usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수 나이", 10);
-		map.put("영희 나이", 20);
-	
-		return map;
-	}
-	
-	@RequestMapping("usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수");
-		list.add("영희");
-		
-		return list;
-	}
-	
-	@RequestMapping("usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1", "내용1");
-		
-		return article;
-	}
-	
-	@RequestMapping("usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		List<Article> articles = new ArrayList<>();
-		articles.add(new Article(1, "제목1", "내용2"));
-		articles.add(new Article(2, "제목2", "내용2"));
-				
-		return articles;
-	}
 }
