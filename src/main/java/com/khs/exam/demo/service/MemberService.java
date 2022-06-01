6e61b17713b0505f6f6a7e17d8a28e887c8541d9
tmedcount <tmedcount@gmail.com>
2022-06-02 02:01:51 +0900

    MemberRepository 추가, 회원가입 처리

--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.khs.exam.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.khs.exam.demo.repository.MemberRepository;
 
 @Service
 public class MemberService {
-	//private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		//memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 
 }
