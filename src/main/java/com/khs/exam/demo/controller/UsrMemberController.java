b12535fbe2dca0ffc41efc64e79f48358b1b6ee1
tmedcount <tmedcount@gmail.com>
2022-06-02 16:59:05 +0900

    회원가입 완료 후, 생성된 회원 출력

--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.khs.exam.demo.service.MemberService;
+import com.khs.exam.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
@@ -14,9 +15,11 @@ public class UsrMemberController {
 
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
-		return "성공";
+		Member member = memberService.getMemberById(id);
+		
+		return member;
 	}
 }
--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.khs.exam.demo.repository.MemberRepository;
+import com.khs.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
 	}
 
 }
