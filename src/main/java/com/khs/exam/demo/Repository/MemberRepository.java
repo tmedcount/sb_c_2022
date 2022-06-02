057d33dcf118af78145d0e39a71bc9be55f49224
tmedcount <tmedcount@gmail.com>
2022-06-02 17:27:44 +0900

    회원가입 시, 중복 로그인 아이디 체크

--- src/main/java/com/khs/exam/demo/Repository/MemberRepository.java
+++ src/main/java/com/khs/exam/demo/Repository/MemberRepository.java
@@ -30,4 +30,10 @@ public interface MemberRepository {
 			WHERE M.id = #{id}
 			""")
 	Member getMemberById(@Param("id") int id);
+
+	@Select("""
+			SELECT * FROM `member` AS M
+			WHERE M.loginId = #{loginId}
+			""")
+	Member getMemberByLoginId(@Param("loginId") String loginId);
 }
--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -15,8 +15,12 @@ public class UsrMemberController {
 
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+		if(id == -1) {
+			return "이미 존재하는 로그인 아이디입니다."; 
+		}
 		
 		Member member = memberService.getMemberById(id);
 		
