7d05d346b860c6d972cfab2d296cbc87515254a9
tmedcount <tmedcount@gmail.com>
2022-06-08 00:33:43 +0900

    UsrMemberController, MemberService에 ResultData Generic 기능 적용

--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -13,7 +13,7 @@ public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		// 로그인 아이디 중복 체크
 		Member oldMember = getMemberByLoginId(loginId);
 		
