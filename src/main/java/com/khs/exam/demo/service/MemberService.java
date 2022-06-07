8adb6aab48fc9256a02d89f6b1ddf5023d712217
tmedcount <tmedcount@gmail.com>
2022-06-07 20:15:33 +0900

    usr/member/doJoin에 ResultData 적용

--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -4,31 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.khs.exam.demo.repository.MemberRepository;
+import com.khs.exam.demo.util.Ut;
 import com.khs.exam.demo.vo.Member;
+import com.khs.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		// 로그인 아이디 중복 체크
 		Member oldMember = getMemberByLoginId(loginId);
 		
 		if(oldMember != null) {
-			return -1;
+			return ResultData.from("F-7", Ut.f("해당 로그인 아이디(%s)는 이미 사용중입니다.", loginId));
 		}
 		
 		// 이름, 이메일 중복 체크
 		oldMember = getMemberByNameAndEmail(name, email);
 		
 		if(oldMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email));
 		}
 		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
